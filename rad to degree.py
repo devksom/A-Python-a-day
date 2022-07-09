035b00b7656c0bec78b9ce6a3a22e3e94ff4ef74
Stephen Somuah <dev@theinfoseclab.com>
2022-07-09 01:03:18 +0000

    Function to Convert Radian to Degree

---
+++ rad to degree.py
@@ -0,0 +1,16 @@
+#Convert radians to degree 1rad × 180/π = 57.296°
+#Receive Input rad
+#Multiply rad by 180
+#Divide product by pi
+#Print result
+#import math library
+import math
+def radconv():
+    inp= float(input('Please enter number to be converted '))
+    pi=math.pi
+    prod = inp*180
+    result=prod/pi
+    print(result)
+    return
+radconv()
+
